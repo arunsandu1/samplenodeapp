bcf1c209419a1e155bcb035f55c2d86499f0c7a4
Arun Sandu <arunsandu666@gmail.com>
2022-07-01 19:31:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.end('<h1>Hello, World Seattle folks!!!</h1>');
+        res.end('<h1>Hello, World Seattle folks here!!!</h1>');
     }
 });
 
